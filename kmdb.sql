1f9a28fa124051424d58e03f2bd2a914c043518f
Alexandra Ibarra <alexandraibarra2025@u.northwestern.edu>
2024-04-17 15:31:10 +0000

    I added the last tables and the drop statements.

--- kmdb.sql
+++ kmdb.sql
@@ -1,5 +1,7 @@
 DROP TABLE IF EXISTS movies;
 DROP TABLE IF EXISTS studios;
+DROP TABLE IF EXISTS actors;
+DROP TABLE IF EXISTS roles;
 
 CREATE TABLE movies (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -14,3 +16,17 @@ CREATE TABLE studios (
   name TEXT
 );
 
+CREATE TABLE actors (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT,
+    last_name TEXT,
+    movie_id INTEGER
+);
+
+CREATE TABLE roles (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    movie_id INTEGER,
+    actor_id INTEGER,
+    first_name TEXT,
+    last_name TEXT
+);
